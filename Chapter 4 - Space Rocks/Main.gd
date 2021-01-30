ac42fc102f4e30098a3ff0f0227bfa515735b840
kylerisse <kylerisse@users.noreply.github.com>
2021-01-30 13:35:38 -0800

    Space Rocks - add HUD

--- Chapter 4 - Space Rocks/Main.gd
+++ Chapter 4 - Space Rocks/Main.gd
@@ -4,13 +4,43 @@ export (PackedScene) var Rock
 
 var screensize = Vector2()
 
+var level = 0
+var score = 0
+var playing = false
+
+func new_game():
+	for rock in $Rocks.get_children():
+		rock.queue_free()
+	level = 0
+	score = 0
+	$HUD.update_score(score)
+	$Player.start()
+	$HUD.show_message("Get Ready!")
+	yield($HUD/MessageTimer, "timeout")
+	playing = true
+	new_level()
+
+func new_level():
+	level += 1
+	$HUD.show_message("Wave %s" % level)
+	for i in range(level):
+		spawn_rock(3)
+
+func game_over():
+	playing = false
+	$HUD.game_over()
+
 func _ready():
 	randomize()
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
 	for i in range(3):
 		spawn_rock(3)
-	
+
+func _process(delta):
+	if playing and $Rocks.get_child_count() == 0:
+		new_level()
+
 func _on_Player_shoot(bullet, pos, dir):
 	var b = bullet.instance()
 	b.start(pos, dir)
@@ -26,3 +56,13 @@ func spawn_rock(size, pos=null, vel=null):
 	r.screensize= screensize
 	r.start(pos, vel, size)
 	$Rocks.add_child(r)
+	r.connect('exploded', self, '_on_Rock_exploded')
+
+func _on_Rock_exploded(size, radius, pos, vel):
+	if size <= 1:
+		return
+	for offset in [-1, 1]:
+		var dir = (pos - $Player.position).normalized().tangent() * offset
+		var newpos = pos + dir * radius
+		var newvel = dir * vel.length() * 1.1
+		spawn_rock(size - 1, newpos, newvel)
